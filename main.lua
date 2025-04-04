5f8accdd5a33fa8413b5990ec5bdf7eccf75841a
Yuzure <lipehhgowner@gmail.com>
2025-04-04 00:42:01 -0300

    Atualizar o main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,9 @@
-local repo = 'Yuzure-sudo/TheusHub'
-local branch = 'main'
-
-local function githubRequest(file)
-    return loadstring(game:HttpGet(('https://raw.githubusercontent.com/%s/%s/%s'):format(repo, branch, file)))()
-end
-
-local status, result = pcall(function()
-    return githubRequest('loader.lua')
+local success, result = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/MuscleLegendsV2.lua"))()
 end)
 
-if status then
-    print('Script carregado com sucesso!')
-else
-    warn('Erro ao carregar o script:', result)
+if not success then
+    warn("Erro ao carregar script: " .. tostring(result))
+    -- Tentativa de backup
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/backup.lua"))()
 end
