6bdca2637f8f3f0e2624fed96d4ea6c12dfeabd1
Yuzure <lipehhgowner@gmail.com>
2025-04-03 22:17:19 -0300

    Atualizar o main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/interface.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/funcoes.lua"))()
+if game.PlaceId == 2753915549 then
+    repeat wait() until game:IsLoaded()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/TheusHub.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Este script só funciona no Blox Fruits!")
+end
