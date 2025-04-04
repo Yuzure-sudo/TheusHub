5c460e5031f3bd8bd9e67b28a4408453cd485f98
Yuzure <lipehhgowner@gmail.com>
2025-04-03 21:34:18 -0300

    Atualizar o funções.lua

--- funções.lua
+++ funções.lua
@@ -1,4 +1,41 @@
--- Quest Data Sea 1 (Níveis)
+-- Variáveis Globais e Configurações
+getgenv().AutoFarm = false
+getgenv().TweenSpeed = 250
+getgenv().AutoFarmBone = false
+getgenv().AutoEliteHunter = false
+getgenv().AutoFactory = false
+getgenv().AutoNewWorld = false
+getgenv().AutoThirdWorld = false
+getgenv().AutoBartilo = false
+getgenv().AutoRengoku = false
+getgenv().AutoEctoplasm = false
+getgenv().AutoBudySword = false
+getgenv().AutoFarmBoss = false
+getgenv().AutoFarmAllBoss = false
+getgenv().AutoFarmFruitMastery = false
+getgenv().AutoFarmGunMastery = false
+getgenv().AutoHolyTorch = false
+getgenv().AutoCombo = false
+getgenv().AutoSeaBeast = false
+getgenv().AutoPole = false
+getgenv().AutoSaber = false
+getgenv().AutoSuperhuman = false
+getgenv().AutoSharkman = false
+getgenv().AutoDeathStep = false
+getgenv().AutoElectricClaw = false
+getgenv().AutoDragonTalon = false
+getgenv().AutoGodhuman = false
+getgenv().AutoStoreFruit = false
+
+-- Serviços
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local TweenService = game:GetService("TweenService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local VirtualUser = game:GetService("VirtualUser")
+local HttpService = game:GetService("HttpService")
+
+-- Quest Data Sea 1
 local QuestData = {
     ["1-9"] = {
         Monster = "Bandit [Lv. 5]",
@@ -12,7 +49,7 @@ local QuestData = {
         Quest = "JungleQuest",
         QuestLv = 1,
         CFrameQuest = CFrame.new(-1604.12012, 36.8521118, 154.23732),
-        CFrameMon = CFrame.new(-1402.74609, 98.5633316, 90.6417007)
+        CFrameMon = CFrame.new(-1772.4093017578, 60.860641479492, 54.872589111328)
     },
     ["15-29"] = {
         Monster = "Gorilla [Lv. 20]",
@@ -89,463 +126,5 @@ local QuestData = {
         Quest = "PrisonerQuest",
         QuestLv = 1,
         CFrameQuest = CFrame.new(5310.61035, 0.350014925, 474.946594),
-        CFrameMon = CFrame.new(5099.17676, 0.350014925, 203.515625)
+        CFrameMon = CFrame.new(5093.04199, -0.144462526, 478.985931)
     },
-    ["210-249"] = {
-        Monster = "Dangerous Prisoner [Lv. 210]",
-        Quest = "PrisonerQuest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(5310.61035, 0.350014925, 474.946594),
-        CFrameMon = CFrame.new(5099.17676, 0.350014925, 203.515625)
-    },
-    ["250-274"] = {
-        Monster = "Toga Warrior [Lv. 250]",
-        Quest = "ColosseumQuest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(-1576.11743, 7.38933945, -2983.30762),
-        CFrameMon = CFrame.new(-1779.97583, 44.6077499, -2736.35474)
-    },
-    ["275-299"] = {
-        Monster = "Gladiator [Lv. 275]",
-        Quest = "ColosseumQuest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(-1576.11743, 7.38933945, -2983.30762),
-        CFrameMon = CFrame.new(-1274.75903, 58.1895943, -3188.16309)
-    },
-    ["300-324"] = {
-        Monster = "Military Soldier [Lv. 300]",
-        Quest = "MagmaQuest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(-5316.55859, 12.2370615, 8517.2998),
-        CFrameMon = CFrame.new(-5363.01123, 41.5056877, 8548.47266)
-    },
-    ["325-374"] = {
-        Monster = "Military Spy [Lv. 325]",
-        Quest = "MagmaQuest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(-5316.55859, 12.2370615, 8517.2998),
-        CFrameMon = CFrame.new(-5787.99023, 120.864456, 8762.25293)
-    },
-    ["375-399"] = {
-        Monster = "Fishman Warrior [Lv. 375]",
-        Quest = "FishmanQuest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(61122.5625, 18.4716396, 1568.16504),
-        CFrameMon = CFrame.new(61163.8516, 5.3073043, 1819.78418)
-    },
-    ["400-449"] = {
-        Monster = "Fishman Commando [Lv. 400]",
-        Quest = "FishmanQuest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(61122.5625, 18.4716396, 1568.16504),
-        CFrameMon = CFrame.new(61163.8516, 5.3073043, 1819.78418)
-    },
-    ["450-474"] = {
-        Monster = "God's Guard [Lv. 450]",
-        Quest = "SkyExp1Quest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(-4721.71436, 845.277161, -1954.20105),
-        CFrameMon = CFrame.new(-4716.95703, 853.089722, -1933.925427)
-    },
-    ["475-524"] = {
-        Monster = "Shanda [Lv. 475]",
-        Quest = "SkyExp1Quest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(-7863.63672, 5545.49316, -379.826324),
-        CFrameMon = CFrame.new(-7685.12354, 5601.05127, -443.171509)
-    },
-    ["525-549"] = {
-        Monster = "Royal Squad [Lv. 525]",
-        Quest = "SkyExp2Quest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(-7902.66895, 5635.96387, -1411.71802),
-        CFrameMon = CFrame.new(-7685.02051, 5606.87842, -1442.729)
-    },
-    ["550-624"] = {
-        Monster = "Royal Soldier [Lv. 550]",
-        Quest = "SkyExp2Quest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(-7902.66895, 5635.96387, -1411.71802),
-        CFrameMon = CFrame.new(-7864.44775, 5661.94092, -1708.22351)
-    },
-    ["625-649"] = {
-        Monster = "Galley Pirate [Lv. 625]",
-        Quest = "FountainQuest",
-        QuestLv = 1,
-        CFrameQuest = CFrame.new(5254.60156, 38.5011406, 4049.69678),
-        CFrameMon = CFrame.new(5595.06982, 41.5013695, 3961.47095)
-    },
-    ["650-699"] = {
-        Monster = "Galley Captain [Lv. 650]",
-        Quest = "FountainQuest",
-        QuestLv = 2,
-        CFrameQuest = CFrame.new(5254.60156, 38.5011406, 4049.69678),
-        CFrameMon = CFrame.new(5658.5752, 38.5361786, 4928.93506)
-    }
-}
-
--- Quests Especiais
-local SpecialQuests = {
-    -- Quest do Shanks
-    ShanksQuest = {
-        PuzzleLocations = {
-            CFrame.new(-1405.19, 29.8521, 4.69435),
-            CFrame.new(-1181.13, 40.5873, -205.189),
-            CFrame.new(-1133.26, 40.5873, -424.431),
-            CFrame.new(-1209.85, 40.5872, -501.310),
-            CFrame.new(-1336.24, 40.5873, -486.073),
-            CFrame.new(-1453.88, 40.5873, -371.214),
-            CFrame.new(-1600.63, 40.5873, -238.263)
-        }
-    },
-    
-    -- Quest do Ken Haki
-    ObservationQuest = {
-        QuestCFrame = CFrame.new(-12444.78515625, 332.40396118164, -7673.1806640625),
-        TrainingPoints = {
-            CFrame.new(-12545.532226563, 337.20260620117, -7505.7006835938),
-            CFrame.new(-12526.278320313, 337.20260620117, -7651.2241210938),
-            CFrame.new(-12449.345703125, 337.20260620117, -7786.1186523438),
-            CFrame.new(-12317.041992188, 337.20260620117, -7784.5986328125)
-        }
-    }
-}
-
--- Função Auto Quest Shanks
-function AutoShanksQuest()
-    if getgenv().AutoShanks then
-        for _, puzzleLocation in ipairs(SpecialQuests.ShanksQuest.PuzzleLocations) do
-            Tween(puzzleLocation)
-            wait(1)
-            fireproximityprompt("Interact") -- Ajuste conforme necessário
-            wait(2)
-        end
-    end
-end
-
--- Função Auto Quest Ken
-function AutoObservationQuest()
-    if getgenv().AutoKenQuest then
-        Tween(SpecialQuests.ObservationQuest.QuestCFrame)
-        wait(1)
-        -- Iniciar quest
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("KenTalk", "Start")
-        
-        for _, point in ipairs(SpecialQuests.ObservationQuest.TrainingPoints) do
-            Tween(point)
-            wait(3)
-        end
-        
-        -- Completar quest
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("KenTalk", "Complete")
-    end
-end-- Funções para Espadas e Artes Marciais
-local FightingStyles = {
-    BlackLeg = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyBlackLeg")
-        end,
-        Requirements = {
-            Level = 20,
-            Beli = 150000,
-        }
-    },
-    DeathStep = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDeathStep")
-        end,
-        Requirements = {
-            Level = 400,
-            Beli = 5000000,
-            BlackLeg = true,
-            ["Pure Diamond"] = 50,
-            ["Fish Tail"] = 50
-        }
-    },
-    Electro = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyElectro")
-        end,
-        Requirements = {
-            Level = 40,
-            Beli = 500000,
-        }
-    },
-    ElectricClaw = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyElectricClaw")
-        end,
-        Requirements = {
-            Level = 400,
-            Beli = 5000000,
-            Electro = true,
-            ["Pure Diamond"] = 50,
-            ["Fish Tail"] = 50
-        }
-    },
-    DragonClaw = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BlackbeardReward", "DragonClaw", "1")
-        end,
-        Requirements = {
-            Level = 300,
-            ["Dragon Scale"] = 3
-        }
-    }
-}
-
--- Funções para Espadas
-local Swords = {
-    Saber = {
-        Quest = function()
-            return game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyItem", "Saber")
-        end,
-        Requirements = {
-            Level = 200,
-            Beli = 1000000,
-        }
-    },
-    Rengoku = {
-        Location = CFrame.new(-400.85889, 24.7645149, 5329.12012),
-        Requirements = {
-            Level = 950
-        }
-    },
-    Pole = {
-        Location = CFrame.new(-7911.14453, 5613.89795, -2272.67822),
-        Requirements = {
-            Level = 450
-        }
-    }
-}
-
--- Funções Auto Buy
-function AutoBuyAbilities()
-    if getgenv().AutoBuyAbilities then
-        -- Comprar Abilities disponíveis
-        local abilities = {
-            "Geppo",
-            "Buso",
-            "Soru",
-            "Ken"
-        }
-        
-        for _, ability in pairs(abilities) do
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyHaki", ability)
-            wait(1)
-        end
-    end
-end
-
--- Auto Buy Fighting Styles
-function AutoBuyFightingStyles()
-    if getgenv().AutoBuyFightingStyles then
-        for style, data in pairs(FightingStyles) do
-            if LocalPlayer.Data.Level.Value >= data.Requirements.Level then
-                data.Quest()
-                wait(1)
-            end
-        end
-    end
-end
-
--- Auto Buy Legendary Swords
-function AutoBuyLegendary()
-    if getgenv().AutoBuyLegendary then
-        local args = {
-            [1] = "LegendarySwordDealer",
-            [2] = "2"
-        }
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-    end
-end
-
--- Fruit Functions
-function AutoRandomFruit()
-    if getgenv().AutoRandomFruit then
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Cousin", "Buy")
-        wait(15) -- Cooldown
-    end
-end
-
-function AutoStoreFruit()
-    if getgenv().AutoStoreFruit then
-        for _, v in pairs(LocalPlayer.Backpack:GetChildren()) do
-            if string.find(v.Name, "Fruit") then
-                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit", v.Name)
-            end
-        end
-    end
-end
-
--- Server Hop Function
-function HopServer()
-    local PlaceID = game.PlaceId
-    local AllIDs = {}
-    local foundAnything = ""
-    local actualHour = os.date("!*t").hour
-    local Deleted = false
-    
-    local File = pcall(function()
-        AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
-    end)
-    
-    if not File then
-        table.insert(AllIDs, actualHour)
-        writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-    end
-    
-    function TPReturner()
-        local Site;
-        if foundAnything == "" then
-            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-        else
-            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-        end
-        
-        local ID = ""
-        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-            foundAnything = Site.nextPageCursor
-        end
-        
-        local num = 0;
-        for i,v in pairs(Site.data) do
-            local Possible = true
-            ID = tostring(v.id)
-            if tonumber(v.maxPlayers) > tonumber(v.playing) then
-                for _,Existing in pairs(AllIDs) do
-                    if num ~= 0 then
-                        if ID == tostring(Existing) then
-                            Possible = false
-                        end
-                    else
-                        if tonumber(actualHour) ~= tonumber(Existing) then
-                            local delFile = pcall(function()
-                                delfile("NotSameServers.json")
-                                AllIDs = {}
-                                table.insert(AllIDs, actualHour)
-                            end)
-                        end
-                    end
-                    num = num + 1
-                end
-                if Possible == true then
-                    table.insert(AllIDs, ID)
-                    wait()
-                    pcall(function()
-                        writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-                        wait()
-                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-                    end)
-                    wait(4)
-                end
-            end
-        end
-    end
-    
-    function Teleport()
-        while wait() do
-            pcall(function()
-                TPReturner()
-                if foundAnything ~= "" then
-                    TPReturner()
-                end
-            end)
-        end
-    end
-    
-    Teleport()
-end
-
--- Auto Triple Katana
-function AutoTripleKatana()
-    if getgenv().AutoTripleKatana then
-        -- Verificar se tem as 3 espadas necessárias
-        local swords = {
-            "Shisui",
-            "Wando",
-            "Saddi"
-        }
-        
-        local hasSwords = true
-        for _, sword in pairs(swords) do
-            if not LocalPlayer.Backpack:FindFirstChild(sword) and not LocalPlayer.Character:FindFirstChild(sword) then
-                hasSwords = false
-                break
-            end
-        end
-        
-        if hasSwords then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TradeTripleKatana")
-        end
-    end
-end
-
--- Função para Auto Rengoku
-function AutoRengoku()
-    if getgenv().AutoRengoku then
-        if LocalPlayer.Data.Level.Value >= Swords.Rengoku.Requirements.Level then
-            Tween(Swords.Rengoku.Location)
-            wait(1)
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyRengoku")
-        end
-    end
-end
-
--- Função para Auto Pole
-function AutoPole()
-    if getgenv().AutoPole then
-        if LocalPlayer.Data.Level.Value >= Swords.Pole.Requirements.Level then
-            Tween(Swords.Pole.Location)
-            wait(1)
-            for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-                if v.Name == "Thunder God" and v.Humanoid.Health > 0 then
-                    repeat
-                        Tween(v.HumanoidRootPart.CFrame * CFrame.new(0,30,0))
-                        v.HumanoidRootPart.Size = Vector3.new(60,60,60)
-                        v.HumanoidRootPart.Transparency = 0.8
-                        v.Humanoid.JumpPower = 0
-                        v.Humanoid.WalkSpeed = 0
-                        v.HumanoidRootPart.CanCollide = false
-                        game:GetService'VirtualUser':CaptureController()
-                        game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
-                        wait()
-                    until v.Humanoid.Health <= 0 or not getgenv().AutoPole
-                end
-            end
-        end
-    end
-end
-
--- Iniciar todas as funções em loops
-spawn(function()
-    while wait() do
-        if getgenv().AutoFarm then
-            AutoFarm()
-        end
-        if getgenv().AutoBuyAbilities then
-            AutoBuyAbilities()
-        end
-        if getgenv().AutoBuyFightingStyles then
-            AutoBuyFightingStyles()
-        end
-        if getgenv().AutoBuyLegendary then
-            AutoBuyLegendary()
-        end
-        if getgenv().AutoRandomFruit then
-            AutoRandomFruit()
-        end
-        if getgenv().AutoStoreFruit then
-            AutoStoreFruit()
-        end
-        if getgenv().AutoTripleKatana then
-            AutoTripleKatana()
-        end
-        if getgenv().AutoRengoku then
-            AutoRengoku()
-        end
-        if getgenv().AutoPole then
-            AutoPole()
-        end
-    end
-end)
