02c9e6c5010bdd28e55282f230086f2874d31211
Yuzure <lipehhgowner@gmail.com>
2025-04-04 02:52:24 -0300

    Delete MuscleLegendsV2.lua

--- MuscleLegendsV2.lua
+++
@@ -1,9 +0,0 @@
-local success, result = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/MuscleLegendsV2.lua"))()
-end)
-
-if not success then
-    warn("Erro ao carregar script: " .. tostring(result))
-    -- Tentativa de backup
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Yuzure-sudo/TheusHub/main/backup.lua"))()
-end
